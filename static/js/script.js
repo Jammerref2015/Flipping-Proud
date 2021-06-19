9cee34890d7dc5e7d1824da86134090ff171a213
Paul Harrington <hello@loosenthedark.tech>
2021-06-19 22:12:52 +0000

    Populate DOM with flipcard content

--- static/js/script.js
+++ static/js/script.js
@@ -1,14 +1,29 @@
 console.log("Flippin' proud!");
 
-const flipCardContainer = document.querySelector(".flip-card-container");
-                flipCardContainer.addEventListener("click", function() {
-                flipCardContainer.classList.toggle("flip")
-            });
+// const flipCardContainer = document.querySelector(".flip-card-container");
+// $(document).on('click', flipCardContainer, function() {
+//     flipCardContainer.classList.toggle("flip");
+// });
 
 $(document).ready(function() {
     console.log("Where's the data?");
     $.get("static/data/data.json", function(data) {
-        console.log(data);
+        for (let i = 0; i < data.length; i++) {
+            let flipCard = "<div class='flip-card-outer'>";
+            flipCard += "<div class='flip-card-container'>";
+
+            let flipCardFront = "<div class='flip-card-front'>";
+            flipCardFront += "<img src='https://upload.wikimedia.org/wikipedia/commons/9/9e/Tim_Cook_%282017%2C_cropped%29.jpg' alt='Tim Cook'>";
+
+            let flipCardBack = "<div class='flip-card-back'>";
+            flipCardBack += "<p>American business executive who has been the chief executive officer of Apple Inc. since 2011. <span>Tim Cook</span></p>";
+
+            flipCard += flipCardFront;
+            flipCard += flipCardBack;
+
+            $("nav").after(flipCard);
+
+        }
     // var i, tr, resort;
     // for (i = 0; i < data.length; i++) {
     //     tr = "<tr>";
