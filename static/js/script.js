74953d4a95ffdebdffc74385db6d0e745f38e90b
Paul Harrington <hello@loosenthedark.tech>
2021-06-21 07:32:21 +0000

    Flipcard CSS finished for desktop, landing page animation eased

--- static/js/script.js
+++ static/js/script.js
@@ -9,13 +9,13 @@ $(document).ready(function() {
 
             let flipCardFront = "<div class='flip-card-front'>";
             flipCardFront += `<img src='${data[i].img_url}' alt='${data[i].name}'>`;
-            flipCardFront += `<div class='d-none d-md-block name-wrapper'><h5 class='pt-2 pl-3 name-wrapper-heading'>${data[i].name}</h5></div>`;
+            flipCardFront += `<div class='d-none d-md-block name-wrapper'><h5 class='pt-2 pt-xl-0 pl-3 name-wrapper-heading'>${data[i].name}</h5></div>`;
             flipCardFront += "<img src='static/images/pride-doughnut-icon.png' width='60' class='icon pride-doughnut-icon' alt='Pride doughnut icon'>";
             flipCardFront += "<img src='static/images/icon-flip-over.svg' width='60' class='icon flip-over-icon' alt='flip over icon'>";
             flipCardFront += "</div>";
 
             let flipCardBack = "<div class='flip-card-back'>";
-            flipCardBack += "<img src='static/images/pride-doughnut-icon.png' width='80' class='icon pride-doughnut-icon pride-doughnut-icon-back mb-2 d-md-none' alt='Pride doughnut icon'>";
+            flipCardBack += "<img src='static/images/pride-doughnut-icon.png' width='80' class='icon pride-doughnut-icon pride-doughnut-icon-back mb-2 mb-xl-0 d-md-none d-xl-block' alt='Pride doughnut icon'>";
             flipCardBack += `<h3 class="mb-0 d-md-none">${data[i].name}</h3>`;
             flipCardBack += `<p>${data[i].bio}</p>`;
             flipCardBack += "</div>";
